33d714e5f4397caf6a0bf842a9fdfe34debf3800
ender019 <ender-fox@mail.ru>
2025-07-22 14:11:00 +0300

    init commit

---
+++ src/main/java/com/unknown/link/X3LinksApplication.java
@@ -0,0 +1,13 @@
+package com.unknown.link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class X3LinksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(X3LinksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unknown/link/X3LinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unknown.link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class X3LinksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
